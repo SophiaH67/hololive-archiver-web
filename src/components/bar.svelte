da7012a24c59eccebbba889835f3ce8613e28be8
marnixah <marnix@hagehoeve.com>
2021-12-04 03:49:17 +0100

    feat: basic navbar

---
+++ src/components/bar.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { settings } from '$lib/stores'
+</script>
+
+<nav class="bg-purple-500 h-12 mb-2 shadow-lg flex px-4 justify-between items-center">
+  <h2 class="text-white text-xl my-auto whitespace-nowrap">Hololive Archiver</h2>
+  <div>
+    <a href="/" class="text-white text-sm">JOBS</a>
+    <a href="/" class="text-white text-sm">OPTIONS</a>
+    <button
+      class="text-white text-sm"
+      on:click={() => {
+        settings.update(settings => ({
+          ...settings,
+          darkMode: !settings.darkMode,
+        }))
+      }}
+      >D</button>
+  </div>
+</nav>
--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -3,4 +3,8 @@ import { persist, localStorage } from "@macfja/svelte-persistent-store";
 import type Job from "src/classes/job";
 
 export const jobs = writable([] as Job[]);
-export const settings = persist(writable({}), localStorage(), "settings");
+export const settings = persist(
+  writable({ darkMode: false }),
+  localStorage(),
+  "settings"
+);
