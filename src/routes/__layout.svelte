41ef1e5d4d3ce1a0a0eb3a434d3791791e35a6c7
marnixah <marnix@hagehoeve.com>
2021-12-04 04:07:13 +0100

    fix: no dark mode when SSR

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,16 +2,19 @@
   import Bar from "../components/bar.svelte";
   import { settings } from "$lib/stores"
 
-  const htmlElement = document.getElementsByTagName("html")[0];
   settings.subscribe(settings => {
+    if (!browser) return
+    const htmlElement = document.getElementsByTagName("html")[0];
     if (settings.darkMode) {
       htmlElement.classList.add("dark");
     } else {
       htmlElement.classList.remove("dark");
     }
   });
+  
 
   import "../app.css";
+import { browser } from "$app/env";
 </script>
 
 <div class="bg-white dark:bg-gray-900 min-h-screen">
