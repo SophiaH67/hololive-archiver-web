a3b5d8befaf0fde2a1354bba5aa672bdc154dc08
marnixah <marnix@hagehoeve.com>
2021-12-05 23:28:12 +0100

    fix(input): ugly text padding/margin

--- src/components/input.svelte
+++ src/components/input.svelte
@@ -5,11 +5,11 @@
 </script>
 
 <div class="w-1/2 px-1">
-  <label class="block text-gray-700 dark:text-gray-400 text-sm font-bold mb-2" for={label}>
+  <label class="block text-gray-700 dark:text-gray-400 text-sm font-bold mb-1 mt-2" for={label}>
     {label}
   </label>
   <input
-    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 dark:bg-gray-700 dark:text-gray-400 dark:border-gray-700 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 disabled:cursor-not-allowed"
+    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 dark:bg-gray-700 dark:disabled:text-gray-400 dark:text-gray-200 dark:border-gray-700 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 disabled:cursor-not-allowed"
     type="text"
     disabled={disabled}
     bind:value
